5f97521d899b7dfc80404a6496f99c6825bc9336
Vid-Da <d.encinas.t@gmail.com>
2015-12-08 19:34:26 +0100

    bootstrap added

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,5 +1,3 @@
-console.log("working");
-
 var map;
 
 $(document).on('ready', function() {
@@ -12,63 +10,65 @@ $(document).on('ready', function() {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
-    
     console.log(myPosition)
+
     createMap(myPosition);
     setupAutocomplete();
-    } 
+  } 
 
-    function onError(err){
-      console.log("What are you using, IE 7??", err);
-    }
-
-    function createMap(position){
-      map = new google.maps.Map($('#map')[0], {
-      	center: position,
-        zoom: 16
-      });
-
-     createMarker(position);
+  function onError(err){
+    console.log("What are you using, IE 7??", err);
   }
-});
+
+  function createMap(position){
+    map = new google.maps.Map($('#map')[0], {
+    	center: position,
+      zoom: 16
+    });
+
+   createMarker(position);
+  }
+
 
 // Markers-----------------
 
+  function createMarker (position) {
+    var marker = new google.maps.Marker({
+    	position: position,
+    	map: map
+    });
 
-function createMarker (position) {
-  var marker = new google.maps.Marker({
-  	position: position,
-  	map: map
-  });
+     var address = markerInfo.formattedAddress;
+   		marker.addListener('click', function(){
+      getInfoWindow(address).open(map, marker);
+    });
+  }
+});
 
-   var address = markerInfo.formattedAddress;
- 		marker.addListener('click', function(){
-    getInfoWindow(address).open(map, marker);
-  });
-}
+  // Autocomplete --------------
 
-// Autocomplete --------------
+  function setupAutocomplete(){
+    var input = $('#get-places')[0];
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function(){
+      var place = autocomplete.getPlace();
+      if (place.geometry.location) {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }else{
+        alert("The place has no location..?")
+      }
+      createMarker(place.geometry.location);
+      console.log(place);
+    });
+  }
 
-function setupAutocomplete(){
-  var input = $('#get-places')[0];
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.addListener('place_changed', function(){
-    var place = autocomplete.getPlace();
-    if (place.geometry.location) {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }else{
-      alert("The place has no location..?")
-    }
-    createMarker(place.geometry.location);
-    console.log(place);
-  });
-}
+  function getInfoWindow(address){
+    return new google.maps.InfoWindow({
+      content: address
+    });
+  }
 
-function getInfoWindow(address){
-  return new google.maps.InfoWindow({
-    content: address
-  });
-}
+
 
 
